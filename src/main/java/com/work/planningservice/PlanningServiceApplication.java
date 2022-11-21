fd7f407b2d6d29553110d4481cf6d8282defc49a
nicoleta <nicoleta_corocea@yahoo.com>
2022-11-21 19:18:52 +0200

    initial commit

---
+++ src/main/java/com/work/planningservice/PlanningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.work.planningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningServiceApplication.class, args);
+	}
+
+}
