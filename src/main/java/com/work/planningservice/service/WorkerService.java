0ccc960e56be60429c6570ab878d39b6365dcd85
nicoleta <nicoleta_corocea@yahoo.com>
2022-11-22 19:24:23 +0200

    fix sonar & format code

--- src/main/java/com/work/planningservice/service/WorkerService.java
+++ src/main/java/com/work/planningservice/service/WorkerService.java
@@ -1,16 +1,18 @@
 package com.work.planningservice.service;
 
+import com.work.planningservice.model.SchedulerException;
 import com.work.planningservice.model.Worker;
 import com.work.planningservice.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WorkerService {
 
-    private WorkerRepository workerRepository;
+    private final WorkerRepository workerRepository;
 
     @Autowired
     public WorkerService(WorkerRepository workerRepository) {
@@ -21,8 +23,13 @@ public class WorkerService {
         return workerRepository.findAll();
     }
 
-    public Worker getByID(Long id){
-      return  workerRepository.findByWorkerId(id).get();
+    public Worker getByID(Long id) {
+        Optional<Worker> worker = workerRepository.findByWorkerId(id);
+        if (worker.isPresent()) {
+            return worker.get();
+        } else {
+            throw new SchedulerException("THe worker with id " + id + " was not found");
+        }
     }
 
     public Worker save(Worker worker) {
